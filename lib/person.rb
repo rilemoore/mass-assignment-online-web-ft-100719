3f573ff7e0d8fa521e4bd03a20ab377fe5337af6
Riley Moore <rilemoore@gmail.com>
2019-10-24 20:59:48 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,6 +3,8 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attribute_hash)
-    
+    attribute_hash.each do |hash_member|
+            
+    end
   end
 end
