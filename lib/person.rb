b6864b90e2e486a468979069253aafaae978f1f0
Riley Moore <rilemoore@gmail.com>
2019-10-24 21:07:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,6 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attribute_hash)
-    attribute_hash.each do |hash_member|
-      self.send("@#{hash_member}=", hash_member)      
-    end
+    attribute_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 end
